33649d6d7c3e0a57e256b0a578a8feaf541ed362
mdsahilhussain <mdsahil.hussain7@gmail.com>
2022-09-28 16:41:13 +0530

    update static pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+
+// *===== Linked React Router DOM =========================
+import { Route, Routes } from 'react-router-dom';
+// *===== Linked Material UI =========================
+import { Box } from '@mui/material';
+
+// *===== Linked Style Sheet =========================
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// *===== import pages =========================
+import { Home, ExerciseDetail } from "./pages";
+// *===== import components =========================
+import {Navbar, Footer} from './components'
+
+
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
 export default App;
